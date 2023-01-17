b8e694d98768a9797db84371aa604916c0a8c772
Ankush Ashok <ankush_gawande@jabil.com>
2023-01-17 23:17:26 +0800

    Effect of return in catch and the set in finally

---
+++ CatchAndFinallyBlock/ClassA.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatchAndFinallyBlock
+{
+    public class ClassA
+    {
+	    int a = 1;
+        public int DoSomething()
+        {
+            try
+            {
+                a++;
+                throw new Exception("somethig is wrong");
+            }
+            catch
+            {
+                a = a + 1;
+                return a;
+            }
+            finally
+            {
+                a = a + 1;
+            }
+        }
+    }
+}
---
+++ CatchAndFinallyBlock/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CatchAndFinallyBlock;
+
+ClassA obj= new ClassA();
+
+Console.WriteLine($"Value Of a : {obj.DoSomething()}");
