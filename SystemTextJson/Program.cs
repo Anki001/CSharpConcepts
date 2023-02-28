ac51a62523b2ee96abdee3c2a11dd15d3ee7c689
Ankush Ashok <ankush_gawande@jabil.com>
2023-02-28 19:57:45 +0800

    Ankush - Json serialization and deserialization

---
+++ SystemTextJson/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.Json;
+using SystemTextJson;
+
+var wetherForCast = new WeatherForecast
+{
+    Date = DateTime.Parse("2019-08-01"),
+    TemperatureCelsius = 25,
+    Summary = "Hot"
+};
+
+string jsonString = JsonSerializer.Serialize(wetherForCast);
+
+Console.WriteLine($"Serialize string is \n {jsonString}");
+
+WeatherForecast? newWetherForcastObj = JsonSerializer.Deserialize<WeatherForecast>(jsonString);
+
+Console.ReadLine();
---
+++ SystemTextJson/WeatherForecast.cs
@@ -0,0 +1,9 @@
+﻿namespace SystemTextJson
+{
+    internal class WeatherForecast
+    {
+        public DateTimeOffset Date { get; set; }
+        public int TemperatureCelsius { get; set; }
+        public string? Summary { get; set; }
+    }
+}
