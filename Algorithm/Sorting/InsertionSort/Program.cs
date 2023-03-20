d40e9776f455f46164c716041c3603bb7a2e1892
Ankush Ashok <ankush_gawande@jabil.com>
2023-03-20 13:08:46 +0800

    Ankush - Insertion sort

---
+++ Algorithm/Sorting/InsertionSort/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] unSortedArray = new int[] { 4, 2, 1, 5, 3 };
+
+Console.WriteLine("Unsorted List:");
+foreach (var item in unSortedArray)
+{
+    Console.Write($"{item} ");
+}
+
+for (int i = 1; i < unSortedArray.Length; i++)
+{
+    var key = unSortedArray[i];
+    var j = i - 1;
+
+    while (j >= 0 && unSortedArray[j] > key)
+    {
+        unSortedArray[j + 1] = unSortedArray[j];
+        j--;
+    }
+    unSortedArray[j + 1] = key;
+}
+
+Console.WriteLine("\nSorted List:");
+
+foreach (var item in unSortedArray)
+{
+    Console.Write($"{item} ");
+}
+
