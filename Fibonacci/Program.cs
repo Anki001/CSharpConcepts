31798b5e3dda6ca5b5a0fda42c43e5a2e00839b1
Ankush Ashok <ankush_gawande@jabil.com>
2023-03-28 10:13:48 +0800

    Ankush - Fibonacci series created function to accept number of terms

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -1,24 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System.ComponentModel.Design;
+
+static void FibonacciSeries(int numTerms)
+{
+    // Create a new array to hold the numbers of fibonacci series
+    int[] fib = new int[numTerms];
+
+    for (int i = 0; i < numTerms; i++)
+    {
+        if (i < 2)
+        {
+            fib[i] = i;
+        }
+        else
+        {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+    }
+
+    for (int i = 0; i < numTerms; i++)
+    {
+        Console.WriteLine($"{fib[i]} ");
+    }
+}
+
 
 int numTerms = 10;
-int[] fib = new int[numTerms];
-
-for (int i = 0; i < numTerms; i++)
-{
-    if (i < 2)
-    {
-        fib[i] = i;
-    }
-    else
-    {
-        fib[i] = fib[i - 1] + fib[i - 2];
-    }
-}
-
 
 Console.WriteLine("Fibonacci Sequence:");
-for (int i = 0; i < numTerms; i++)
-{
-    Console.WriteLine($"{fib[i]} ");
-}
+FibonacciSeries(numTerms);
