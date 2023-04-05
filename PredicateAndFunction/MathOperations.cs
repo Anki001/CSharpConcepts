1ad75370518b902c15b7ff8cd7b428a20ff185d8
Ankush Ashok <ankush_gawande@jabil.com>
2023-04-05 23:34:22 +0800

    Ankush - Custom Predicate and Function

---
+++ PredicateAndFunction/MathOperations.cs
@@ -0,0 +1,10 @@
+﻿namespace PredicateAndFunction
+{
+    public class MathOperations
+    {
+        public static int MathOps(int a, int b, Func<int, int, int> ops)
+        {
+            return ops(a, b);
+        }
+    }
+}
--- PredicateAndFunction/Program.cs
+++ PredicateAndFunction/Program.cs
@@ -2,6 +2,8 @@
 
 #region [Example of Predicate - Find even number]
 
+using PredicateAndFunction;
+
 List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 Predicate<int> isEven = n => n % 2 == 0;
 
@@ -18,12 +20,35 @@ foreach (var item in evenNumbers)
 
 #region [Example of Function - List is having all even number]
 
-List<int> numbers1 = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 Func<int, bool> isEven1 = x => x % 2 == 0;
 
-var AllEven = numbers1.All(isEven1);
+var AllEven = numbers.All(isEven1);
 
 Console.WriteLine($"All numbers are even: {AllEven}");
 
 #endregion
 
+#region [Custom Func<T, T, TResult> - Math operations]
+var sum = MathOperations.MathOps(2, 4, (x, y) => x + y);
+Console.WriteLine($"Sum is : {sum}");
+
+var sub = MathOperations.MathOps(8, 4, (x, y) => x - y);
+Console.WriteLine($"Substraction is: {sub}");
+
+var multiply = MathOperations.MathOps(2, 4, (x, y) => x * y);
+Console.WriteLine($"Multiply is: {multiply}");
+
+#endregion
+
+#region [Custom Predicate<T> - List operations]
+
+var evenNums = ListOperations.ListOps(numbers, x => x % 2 == 0);
+
+Console.WriteLine("Custom Predicate - Even numbers are:");
+
+foreach (var num in evenNums)
+{
+    Console.WriteLine(num);
+}
+
+#endregion
